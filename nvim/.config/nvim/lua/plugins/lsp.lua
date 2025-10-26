412084df6cae80c74b86524d82b92ebd339bb596
Andrew Truong <adh.truong@gmail.com>
2025-10-26 12:22:55 +0000

    feat: add basedpyright config

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,11 +1,21 @@
 return {
   "neovim/nvim-lspconfig",
-  init = function()
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    -- Disable the K key for hover
-    keys[#keys + 1] = { "K", false }
-  end,
   opts = {
     inlay_hints = { enabled = false },
+    servers = {
+      basedpyright = {
+        settings = {
+          basedpyright = {
+            analysis = {
+              typeCheckingMode = "standard",
+            },
+          },
+        },
+        keys = {
+          -- Disable the K key for hover
+          { "K", false },
+        },
+      },
+    },
   },
 }
