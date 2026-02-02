a16c3f5e5713f701e3cdd0d0d63680fbae880a0b
Andrew Truong <adh.truong@gmail.com>
2026-02-02 08:00:42 +0000

    fix: update nvim python config

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Use basedpyright instead of pyright
+vim.g.lazyvim_python_lsp = "basedpyright"
+
 -- Use ripgrep for :grep command
 vim.opt.grepprg = "rg --vimgrep --no-heading --smart-case"
 vim.opt.grepformat = "%f:%l:%c:%m"
